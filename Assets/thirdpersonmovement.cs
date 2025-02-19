5afdf3dca69820edd1be24e449dea095cddd355c
LHCPlayz <botesjovan@gmail.com>
2025-02-19 13:58:52 +0100

    Camera moves around player with mouse input

---
+++ Assets/thirdpersonmovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class thirdpersonmovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
